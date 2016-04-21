4d88222d15a433bc2607d097c7133b8779bc89b5
Michael Herman <hermanmu@gmail.com>
2016-04-20 18:20:44 -0600

    added story 1 parts 2 and  3

--- project/server/__init__.py
+++ project/server/__init__.py
@@ -9,6 +9,7 @@ import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bcrypt import Bcrypt
+from flask.ext.login import LoginManager
 
 
 ############
@@ -31,9 +32,34 @@ app.config.from_object(app_settings)
 ################
 
 bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 db = SQLAlchemy(app)
 
 from project.server.models import User
+
+
+################
+#  blueprints  #
+################
+
+from project.server.user.views import user_blueprint
+app.register_blueprint(user_blueprint, url_prefix='/auth')
+
+
+#################
+#  flask-login  #
+#################
+
+from project.server.models import User
+
+login_manager.login_view = "user.login"
+login_manager.login_message_category = 'danger'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter(User.id == int(user_id)).first()
 
 
 ############
