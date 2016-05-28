8ccece3463737bd8613282c74840d19829a0ef60
Michael Herman <hermanmu@gmail.com>
2016-05-28 06:58:50 -0600

    story 8 part 1

--- project/client/static/main.js
+++ project/client/static/main.js
@@ -1,5 +1,8 @@
 // custom javascript
 
-$( document ).ready(function() {
+$(document).ready(function() {
   console.log('Sanity Check!');
+  $("#list")
+    .rating({step:1, size:'xs', showClear:false, showCaption:false})
+    .rating('update', 3).val();
 });
